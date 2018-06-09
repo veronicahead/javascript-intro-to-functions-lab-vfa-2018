01f2ca4ab940a176ea8fa6b6d1d317131400adae
Veronica Head <veronica.leigh.head@gmail.com>
2018-06-09 04:51:10 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,20 +14,6 @@ function logWhisper(string) {
   console.log(string.toLowerCase())
 }
 
-describe('sayHiToGrandma(string)', function() {
-  it('returns "I can\'t hear you!" if `string` is lowercase', function() {
-    expect(sayHiToGrandma('hello')).toEqual("I can't hear you!")
-  })
-
-  it('returns "YES INDEED!" if `string` is uppercase', function() {
-    expect(sayHiToGrandma('HELLO')).toEqual("YES INDEED!")
-  })
-
-  it('returns "I love you, too." if `string` is "I love you, Grandma."`', function() {
-    expect(sayHiToGrandma("I love you, Grandma.")).toEqual("I love you, too.")
-  })
-})
-
 function sayHiToGrandma(string){
   if (string.toLowerCase() === string) {
     string = "I can't hear you!"
