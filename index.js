204c14b80cc46a0aaf09b23a3f9e2e925a1b3f24
Veronica Head <veronica.leigh.head@gmail.com>
2018-06-09 04:47:18 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -29,6 +29,13 @@ describe('sayHiToGrandma(string)', function() {
 })
 
 function sayHiToGrandma(string){
-  if string.toLowerCase() === string
-  return
+  if (string.toLowerCase() === string) {
+    return "I can't hear you!"
+  }
+  if (string.toUpperCase() === string) {
+    return "YES INDEED!"
+  }
+  if (string === "I love you, Grandma") {
+    return "I love you, too."
+  }
 }
